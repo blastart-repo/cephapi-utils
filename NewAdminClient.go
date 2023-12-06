32f9717d2da28021fb9f385e48ef6c4f7bdb9847
Ivo Väliste <ivo.valiste@gmail.com>
2023-12-06 19:59:26 +0200

    connection struct methods

--- NewAdminClient.go
+++ NewAdminClient.go
@@ -3,39 +3,36 @@ package cautils
 import (
 	"context"
 	"fmt"
-
 	"github.com/blastart-repo/cephapi-utils/proto"
 	"github.com/ceph/go-ceph/rgw/admin"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type Cluster struct {
-	ClusterName  string `json:"cluster_name" gorm:"unique,primaryKey"`
-	AccessKey    string `json:"access_key"`
-	AccessSecret string `json:"access_secret"`
-	EndpointURL  string `json:"endpoint_url" gorm:"unique"`
+type Connection struct {
+	Address string
+	Port    string
 }
 
-func NewAdminClient(clusterName, address, port string) (*admin.API, error) {
-	resp, err := GetClusterInfo(clusterName, address, port)
-	if err != nil {
-		return nil, err
+func NewConn(address, port string) *Connection {
+	return &Connection{
+		Address: address,
+		Port:    port,
 	}
-	fmt.Println(resp)
-	c, err := admin.New(resp.GetEndpointurl(), resp.GetAccesskey(), resp.GetAccesssecret(), nil)
-	if err != nil {
-		return nil, err
-	}
-	return c, nil
-
 }
 
-func GetClusterInfo(clusterName, address, port string) (*proto.Cluster, error) {
-	conn, err := ConnectgRPC(address, port)
+func (c *Connection) GetClusterInfo(clusterName string) (*proto.Cluster, error) {
+	conn, err := c.ConnectgRPC()
 	if err != nil {
 		return nil, err
 	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}(conn)
+
 	client := proto.NewClusterServiceClient(conn)
 
 	clr, err := client.GetCluster(context.Background(), &proto.ClusterIn{Clustername: clusterName})
@@ -43,22 +40,29 @@ func GetClusterInfo(clusterName, address, port string) (*proto.Cluster, error) {
 		return nil, err
 	}
 
-	defer func(conn *grpc.ClientConn) {
-		err := conn.Close()
-		if err != nil {
-			return
-		}
-	}(conn)
-
 	return clr, nil
 }
 
-func ConnectgRPC(address, port string) (*grpc.ClientConn, error) {
-	serverAddress := fmt.Sprintf("%s:%s", address, port)
+func (c *Connection) ConnectgRPC() (*grpc.ClientConn, error) {
+	serverAddress := fmt.Sprintf("%s:%s", c.Address, c.Port)
 	conn, err := grpc.Dial(serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
 
 	return conn, nil
+}
+
+func (c *Connection) NewAdminClient(clusterName string) (*admin.API, error) {
+	resp, err := c.GetClusterInfo(clusterName)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := admin.New(resp.GetEndpointurl(), resp.GetAccesskey(), resp.GetAccesssecret(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
